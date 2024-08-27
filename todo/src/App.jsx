98a023cbeabc7172a973d741f4c16457b6acd6fa
Mohan Madheshwaran <mmadhesh@global.sial.com>
2024-08-27 18:23:43 +0530

    styling updated

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import "./App.css";
+import { EditIcon, TrashIcon } from "./svg";
 
 function App() {
+  // user input data
   const [inputValue, setInputValue] = useState("");
 
+  // to get index for editing data
+  const [selected, setSelected] = useState();
+
+  // UI list
   const [showData, setShowData] = useState([]);
 
+  // button name
   const [btnAdd, setBtnAdd] = useState(true);
 
   const handleChange = (e) => {
@@ -13,10 +20,20 @@ function App() {
   };
 
   const getData = () => {
-    console.log(inputValue);
+    // console.log(inputValue);
+    // console.log(selected);
 
-    // to push new data to the previous value
-    setShowData((prev) => [...prev, inputValue]);
+    // if edit selected, get index and return updated value
+    if (selected != undefined) {
+      setShowData((prev) => {
+        const copy = [...prev];
+        copy.splice(selected, 1, inputValue);
+        return copy;
+      });
+    } else {
+      // to push new data to the previous value
+      setShowData((prev) => [...prev, inputValue]);
+    }
 
     // to reset the input to empty
     setInputValue("");
@@ -26,12 +43,14 @@ function App() {
   };
 
   const handleDelete = (index) => {
-    // const newData = [...showData];
-    // newData.splice(index, 1);
-    // setShowData(newData);
+    setShowData((prev) => {
+      const copy = [...prev];
+      copy.splice(index, 1);
+      return copy;
+    });
 
     // removing the selected index and showing remaining data in new array
-    setShowData((prev) => prev.filter((v, i) => i !== index));
+    // setShowData((prev) => prev.filter((v, i) => i !== index));
   };
 
   const handleEdit = (index) => {
@@ -40,40 +59,64 @@ function App() {
     // to get the clicked indexed data and setting it to input value
     setInputValue(showData[index]);
 
+    // to send the index
+    setSelected(index);
+
     // to change button to UPDATE
     setBtnAdd(false);
   };
 
   return (
-    <div className="m-10">
-      <input
-        type="text"
-        name="userInput"
-        value={inputValue}
-        onChange={handleChange}
-      />
-      <button type="submit" className="ml-2 btn" onClick={getData}>
-        {btnAdd ? "ADD" : "UPDATE"}
-      </button>
+    <div className="p-8 my-10 mx-auto w-2/5 bg-white shadow-md rounded">
+      <h1 className="flex justify-center mb-6">TO-DO APP</h1>
 
-      <ul className="text-2xl font-bold m-6 w-1/4">
+      <div className="flex items-end mb-6">
+        <div className="flex flex-col w-full">
+          <label className="mb-1">Enter To-Do Item</label>
+          <input
+            type="text"
+            name="userInput"
+            value={inputValue}
+            onChange={handleChange}
+          />
+        </div>
+
+        <button type="submit" className="ml-2 btn" onClick={getData}>
+          {btnAdd ? "ADD" : "UPDATE"}
+        </button>
+      </div>
+
+      <ul>
         {showData.map((data, index) => (
-          <li key={index} className="flex items-center">
-            <div className="bg-red-500 p-2 m-2 min-w-40">
-              {data}({index})
+          <li
+            key={index}
+            className="flex items-center justify-between 
+            border-b border-solid border-gray-400 p-2 my-4"
+          >
+            <div>
+              <span className="text-sm">{index + 1}.</span>
+              <span className="ml-2 text-xl font-semibold">
+                {data}
+                {/* ({index}) */}
+              </span>
             </div>
-            <span
-              className="p-2 bg-gray-300 hover:bg-gray-400 cursor-pointer rounded"
-              onClick={() => handleEdit(index)}
-            >
-              E
-            </span>
-            <span
-              className="p-2 bg-gray-300 hover:bg-gray-400 cursor-pointer rounded ml-2"
-              onClick={() => handleDelete(index)}
-            >
-              D
-            </span>
+
+            <div className="flex">
+              <span
+                title="Edit"
+                className="action-icon"
+                onClick={() => handleEdit(index)}
+              >
+                <EditIcon />
+              </span>
+              <span
+                title="Remove"
+                className="action-icon"
+                onClick={() => handleDelete(index)}
+              >
+                <TrashIcon />
+              </span>
+            </div>
           </li>
         ))}
       </ul>
